6c2293d716230cc79712f618ee2923e499a45ae3
dahechma <99440012+dahechma@users.noreply.github.com>
2023-05-22 00:48:44 +0100

    Add files via upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,15 +9,22 @@ pipeline {
 
       stage('Build Image') {
          steps {
-           withCredentials([usernamePassword(credentialsId: '39f96646-101f-41c0-bf74-6a75c3e95a48	', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-               script {
-                   docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
-                       def image = docker.build("mohamedamine/tuto:1.2", ".")
-                       image.push()
-                   }
-               }
-           }
+           bat '''
+           docker build -t mohamedamined/tuto:1.2 .
+           '''
          }
       }
+
+      stage('Push Image') {
+         steps {
+           bat '''
+           docker tag mohamedamined/tuto:1.2 mohamedamined/tuto:1.2
+           docker login -u dahechamine -p 12345678sS
+           docker push allayayahya/tuto:1.2
+           '''
+         }
+      }
+
    }
 }
+
