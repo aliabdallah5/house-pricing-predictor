f943a13ec64e9f5c15e60e95e44baa55d7085089
dahechma <99440012+dahechma@users.noreply.github.com>
2023-05-21 16:18:03 +0100

    Add files via upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -19,7 +19,7 @@ pipeline {
 
       stage('Push Image') {
          steps {
-           withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+           withCredentials([usernamePassword(credentialsId: '6fb88174-6d35-460d-aaa0-05886517b720', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
                sh '''
                docker tag mohamedamine/tuto:1.2  mohamedamine/tuto:1.2
                docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
@@ -31,3 +31,6 @@ pipeline {
 
    }
 }
+
+
+
