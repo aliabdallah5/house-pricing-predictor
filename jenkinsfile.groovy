893a1aa644418da491eeae4205da9a4ba9d61332
dahechma <99440012+dahechma@users.noreply.github.com>
2023-05-22 00:30:09 +0100

    Add files via upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,24 +10,14 @@ pipeline {
       stage('Build Image') {
          steps {
            withCredentials([usernamePassword(credentialsId: '6fb88174-6d35-460d-aaa0-05886517b720', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-               bat '''
-               docker build -t mohamedamine/tuto:1.2 .
-               '''
+               script {
+                   docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+                       def image = docker.build("mohamedamine/tuto:1.2", ".")
+                       image.push()
+                   }
+               }
            }
          }
       }
-
-      stage('Push Image') {
-         steps {
-           withCredentials([usernamePassword(credentialsId: '6fb88174-6d35-460d-aaa0-05886517b720', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-               bat '''
-               docker tag mohamedamine/tuto:1.2  mohamedamine/tuto:1.2
-               docker login -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
-               docker push mohamedamine/tuto:1.2
-               '''
-           }
-         }
-      }
-
    }
 }
